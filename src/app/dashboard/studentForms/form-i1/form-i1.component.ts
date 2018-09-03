9a4c293cb23885f10ae2dd7430ebf6cb09de77bb
sachin lahiru <sachin.lahiru@gmail.com>
2018-09-03 17:58:48 +0530

    form I-1 web form creation

---
+++ src/app/dashboard/studentForms/form-i1/form-i1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-i1',
+  templateUrl: './form-i1.component.html',
+  styleUrls: ['./form-i1.component.css']
+})
+export class FormI1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
