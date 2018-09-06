55c5592bcf4f233987a57112e818693f414a6973
Sameer3079 <smrbasil4@gmail.com>
2018-09-06 18:57:19 +0530

    Created the viva-schedules and schedule component, created table to show hardcoded dataset of student records

--- src/app/dashboard/viva-schedules/viva-schedules.component.ts
+++ src/app/dashboard/viva-schedules/viva-schedules.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-viva-schedules',
   templateUrl: './viva-schedules.component.html',
   styleUrls: ['./viva-schedules.component.css']
 })
-export class VivaSchedulesComponent implements OnInit {
+export class VivaSchedulesComponent {
 
   constructor() { }
+
+  students = [
+    { studentId: 'IT16005372', name: 'Sameer', role: 'Trainee Software Developer' },
+  ];
 
   ngOnInit() {
   }
