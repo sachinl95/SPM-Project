2b15e8ea223a288c13c86a62fb06bfa0a5fdf929
sajith Priyankara <sajith1995s@gmail.com>
2018-09-09 11:09:46 +0530

    Changed sidebar options

--- src/app/sidebar/sidebar-routes.config.ts
+++ src/app/sidebar/sidebar-routes.config.ts
@@ -2,13 +2,11 @@ export const ROUTES = [
     // { path: '/dashboard', title: 'Dashboard', icon: 'dashboard', children: null },
     { path: 'student', title: 'student', icon: 'person', children: null },
     // { path: 'supervisor', title: 'supervisor', icon: 'supervisor_account', children: null },
-    { path: '#forms', id: 'forms', title: 'Form I-1 I-6', icon: 'apps', children: [
-        {path: 'forms/formI1P1', title: 'Form I-1', icon: 'PT'}, 
-        {path: 'forms/form-i6', title: 'Form I-6', icon: 'I6'},
-      ]},
+    {path: 'forms/formI1P1', title: 'Form I-1', icon: 'apps', children: null }, 
+        {path: 'forms/form-i6', title: 'Form I-6', icon: 'apps', children: null },
       { path: '#formI3', id: 'formI3', title: 'Form I-3', icon: 'apps', children: [
-        {path: 'forms/formI3', title: 'Intern Info', icon: 'PT'},
-        {path: 'forms/formI3/dailyDiary', title: 'Dialy Diary', icon: 'PT'}
+        {path: 'forms/formI3', title: 'Intern Info', icon: 'II'},
+        {path: 'forms/formI3/dailyDiary', title: 'Dialy Diary', icon: 'DD'}
     ] },
     // { path: 'notification', title: 'Notification', icon: 'notifications', children: null },
     // { path: 'alert', title: 'Sweet Alert', icon: 'warning', children: null },
