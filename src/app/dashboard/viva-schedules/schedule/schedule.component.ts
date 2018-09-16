fdd62c1432ec544d369efbc917ac82777fae216c
Sameer3079 <smrbasil4@gmail.com>
2018-09-16 22:38:12 +0530

    Working on the viva schedule interface

--- src/app/dashboard/viva-schedules/schedule/schedule.component.ts
+++ src/app/dashboard/viva-schedules/schedule/schedule.component.ts
@@ -1,21 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.css']
 })
+
 export class ScheduleComponent implements OnInit {
   studentId: String
   private sub: any
+  private title: String
+  private hour: number
+  private minute: number
+  private amPm: String
 
-  constructor(private route: ActivatedRoute) { }
+  private scheduleClass = class {
+    studentId: String
+    dateTime: Date
+    location: String
+  }
+
+  private scheduleObj = new this.scheduleClass()
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.studentId = params['studentId']
     })
+    if (this.studentId === undefined) {
+      this.title = "Schedule Viva for Student"
+    }
+    else {
+      this.title = "Modify Scheduled Viva"
+      this.http.get('', ).subscribe(
+
+      )
+    }
   }
 
 }
