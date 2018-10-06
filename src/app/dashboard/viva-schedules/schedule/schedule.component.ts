6fce1c0edb7d7a374c93afe828bd08e12e877437
Sameer3079 <smrbasil4@gmail.com>
2018-10-06 19:51:34 +0530

    Fixed a bug where the time entered is not the same as what is displayed in the table

--- src/app/dashboard/viva-schedules/schedule/schedule.component.ts
+++ src/app/dashboard/viva-schedules/schedule/schedule.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { HttpClient } from '@angular/common/http'
 import { constants } from '../../../_helpers/appConstants'
+import axios from 'axios'
 
 @Component({
     selector: 'app-schedule',
@@ -15,7 +16,6 @@ export class ScheduleComponent implements OnInit {
     private actionText: String
     private hour: number
     private minute: number
-    private amPm: String
 
     private scheduleClass = class {
         studentId: String
@@ -48,6 +48,51 @@ export class ScheduleComponent implements OnInit {
                 }
             )
         }
+    }
+
+    addOrUpdate() {
+        let method = ''
+        let url = ''
+        if (this.studentId === undefined) {
+            url = constants.HOME_URL + 'admin/schedule-session'
+            method = 'POST'
+        } else {
+            url = constants.HOME_URL + 'admin/schedules/' + this.studentId
+            method = 'PUT'
+        }
+        let temp = new Date(this.scheduleObj.dateTime)
+        let month: any
+        let date: any
+        let hour: any
+        let minute: any
+        month = temp.getMonth() + 1
+        date = temp.getDate()
+        hour = this.hour
+        minute = this.minute
+        if (month < 10) { month = '0' + month }
+        if (date < 10) { date = '0' + date }
+        if (hour < 10) { hour = '0' + hour }
+        if (minute < 10) { minute = '0' + minute }
+        let dateTime = temp.getUTCFullYear() + '-' + month + '-' + date
+            + 'T' + hour + ':' + minute + ':00.000Z'
+        let payload = {
+            studentId: this.studentId,
+            dateTime: dateTime,
+            location: this.scheduleObj.location
+        }
+        axios({
+            method: method,
+            url: url,
+            data: payload
+        }).then(response => {
+            if (this.studentId === undefined) {
+                alert('The viva has been scheduled')
+            } else {
+                alert('The viva schedule has been updated')
+            }
+        }).catch(error => {
+            alert(error.response.data.message)
+        })
     }
 
 }
--- src/app/dashboard/viva-schedules/viva-schedules.component.ts
+++ src/app/dashboard/viva-schedules/viva-schedules.component.ts
@@ -25,21 +25,6 @@ export class VivaSchedulesComponent {
     )
   }
 
-  formatAMPM(date) {
-    let dateString = date
-    date = new Date(date)
-    var hours = date.getHours()
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am'
-    hours = hours % 12
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0' + minutes : minutes
-    var strTime = hours + ':' + minutes + ' ' + ampm
-    let dateArray = dateString.split("T")
-    let dateOnly = dateArray[0]
-    return dateOnly + ' ' + strTime
-  }
-
   rowClickEvent(studentId) {
     alert(studentId + "  " + this.getServerURL())
     let URL = 'http://' + this.getServerURL() + ''
