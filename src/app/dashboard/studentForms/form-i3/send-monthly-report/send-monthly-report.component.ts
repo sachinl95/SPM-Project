40437207b3fd3edea7ddad2f9e0b988c8682a463
sajith Priyankara <sajith1995s@gmail.com>
2018-10-07 09:51:06 +0530

    Get Monthly Report Details

--- src/app/dashboard/studentForms/form-i3/send-monthly-report/send-monthly-report.component.ts
+++ src/app/dashboard/studentForms/form-i3/send-monthly-report/send-monthly-report.component.ts
@@ -1,25 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-send-monthly-report',
@@ -30,19 +13,42 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 
 export class SendMonthlyReportComponent implements OnInit {
-  splayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  // dataSource = new MatTableModule(ELEMENT_DATA);
+  constructor(private http: Http, private router: Router) { }
 
-  applyFilter(filterValue: string) {
-    // this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-  constructor() { }
+  report = []
 
   ngOnInit() {
-    
+    this.http.get('http://localhost:3000/student/dailydiary').map(res => res.json()).subscribe(
+      success => {
+        this.report = success.data
+      },
+      error => {
+        alert(error)
+      }
+    )
   }
 
-}
+  rowClickEvent(studentId) {
+    alert(studentId + "  " + this.getServerURL())
+    let URL = 'http://' + this.getServerURL() + ''
+  }
+
+  getServerURL() {
+    let URLArray = window.location.href.split("/")
+    return URLArray[2]
+  }
+
+  goToSchedule(studentId) {
+    this.router.navigate(['admin/viva-schedules/schedule/:studentId', studentId])
+  }
+
+  normalJavascript(studentId) {
+    window.location.href = 'http://' + this.getServerURL() + '/dashboard/admin/viva-schedules/schedule/' + studentId
+  }
+
+  }
+
+
 
 
 
